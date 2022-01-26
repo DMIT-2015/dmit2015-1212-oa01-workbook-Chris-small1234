2007488ff8fcaa50f6d3b62dd75f67b7df25eb3a
chris-small1234 <smallchris19@gmail.com>
2022-01-26 11:43:30 -0700

    JPA Project Update 01/26/22

--- dmit2015-1212-0a01-jpa-demo/src/main/java/common/config/ApplicationConfig.java
+++ dmit2015-1212-0a01-jpa-demo/src/main/java/common/config/ApplicationConfig.java
@@ -7,20 +7,21 @@ import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.annotation.sql.DataSourceDefinitions;
 
 @DataSourceDefinitions({
-	@DataSourceDefinition(
-		name="java:app/datasources/h2databaseDS",
-		className="org.h2.jdbcx.JdbcDataSource",
-		url="jdbc:h2:file:~/dmit2015db",
-//        url="jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-		user="sa",
-		password="sa"),
-
 //	@DataSourceDefinition(
-//		name="java:app/datasources/hsqldatabaseDS",
-//		className="org.hsqldb.jdbc.JDBCDataSource",
-//		url="jdbc:hsqldb:file:~/databases/dmit2015-1212-assignment02-hsqldb;shutdown=true",
-//		user="user2015",
-//		password="Password2015"),
+//		name="java:app/datasources/h2databaseDS",
+//		className="org.h2.jdbcx.JdbcDataSource",
+//		url="jdbc:h2:file:~/dmit2015db",
+//        url="jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+//		user="sa",
+//		password="sa"),
+
+		@DataSourceDefinition(
+				name="java:app/datasources/hsqldatabaseDS",
+				className="org.hsqldb.jdbc.JDBCDataSource",
+      	url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-demos-hsqldb;shutdown=true",
+//				url="jdbc:hsqldb:mem:dmit2015hsqldb",
+				user="user2015",
+				password="Password2015"),
 
 //	@DataSourceDefinition(
 //		name="java:app/datasources/mssqlDS",
